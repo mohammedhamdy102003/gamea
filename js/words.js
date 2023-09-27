32f9e944fb6497faa53326417d0e2b0d1b8fe7ae
mohammed <hamdy301070@gmail.com>
2023-09-27 21:26:27 +0300

    massage

---
+++ js/words.js
@@ -0,0 +1,56 @@
+let words = [
+    {
+        word: "Earth",
+        hint: "Our home planet with land, water, animals, and amazing nature"
+    },
+    {
+        word: "Sun",
+        hint: "A big, yellow star that gives us light and warmth."
+    },
+    {
+        word: "Moon",
+        hint: " A bright, round rock in the sky that orbits Earth"
+    },
+    {
+        word: "eclipse",
+        hint: "occurs when one heavenly body such as a moon or planet moves into the shadow of another heavenly body"
+    },
+    {
+        word: "Solar eclipse",
+        hint: "A special event when the Moon moves in front of the Sun, creating darkness "
+    },
+    {
+        word: "Lunar Eclipse",
+        hint: "A magical moment when the Earth comes between the Sun and the Moon, casting a shadow"
+    },
+    {
+        word: "Annular Solar Eclipse",
+        hint: " A rare event where the Moon covers the Sun, leaving a shining ring around"
+    },
+    {
+        word: "Super Moon",
+        hint: "A special night when the Moon looks bigger and brighter than usual"
+    },
+    {
+        word: "Eclipse Glasses",
+        hint: "Special glasses that protect our eyes while watching the Sun during an eclipse"
+    },
+  
+    {
+        word: "galaxy",
+        hint: " a vast collection of stars, planets, and cosmic wonders held together by gravity"
+    },
+    {
+        word: "astronomer",
+        hint: "a scientist who studies the stars, planets, and other celestial objects in our universe"
+    },
+    {
+        word: "The umbra",
+        hint: " the darkest part of a shadow, creating a complete shadow. It makes eclipses amazing"
+    },
+    {
+        word: "NASA",
+        hint: " United States government agency that is responsible for science and technology related to air and space"
+    },
+   
+]
